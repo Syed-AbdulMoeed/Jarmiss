c947ad72906b8302f5ad878022067c8aef99deaa
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-08-16 21:58:22 +0200

    Defined sandbox tool with e2b

---
+++ AgentTools/code_interpreter.py
@@ -0,0 +1,14 @@
+from e2b_code_interpreter import Sandbox
+from llama_index.core.tools import FunctionTool
+
+# Define the tool
+def execute_python(code: str):
+    with Sandbox.create() as sandbox:
+        execution = sandbox.run_code(code)
+        return execution.text
+
+e2b_sandbox_tool = FunctionTool.from_defaults(
+    name="execute_python",
+    description="Execute python code in a Jupyter notebook cell and return result",
+    fn=execute_python
+)

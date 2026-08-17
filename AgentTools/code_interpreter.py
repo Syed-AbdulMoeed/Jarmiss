0aaf58b2c39ae68e604a040df90e57f692adde07
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-08-17 20:32:24 +0200

    Initialized dotenv and changed discription

--- AgentTools/code_interpreter.py
+++ AgentTools/code_interpreter.py
@@ -1,5 +1,8 @@
 from e2b_code_interpreter import Sandbox
 from llama_index.core.tools import FunctionTool
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define the tool
 def execute_python(code: str):
@@ -10,7 +13,8 @@ def execute_python(code: str):
 def execute_python_tool():
     e2b_sandbox_tool = FunctionTool.from_defaults(
         name="execute_python",
-        description="Execute python code in a Jupyter notebook cell and return result",
+        description="Execute python code in a Jupyter notebook cell and return result. CRITICAL: DO NOT USE PRINT TO VIEW THE FINAL ANSWER",
         fn=execute_python
     )
     return e2b_sandbox_tool
+

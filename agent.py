e564f61f2b806700c995bfc0f656974b85ae7546
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-08-17 20:32:55 +0200

    Added Streaming agent thoughts: TO-DO: fix MALFORMED_FUNCTION_CALLs

--- agent.py
+++ agent.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import sys
+import asyncio
 from llama_index.llms.google_genai import GoogleGenAI
-from llama_index.core.agent import ReActAgent
+from llama_index.core.agent.workflow import ReActAgent, AgentStream, ToolCallResult  
 # Tools
 from AgentTools.code_interpreter import execute_python_tool
 
@@ -17,19 +18,22 @@ class Jarmiss:
         try:
 
             # Get safe code interpreter tool
-            code_interpreter = execute_python_tool()
-            tools = code_interpreter.to_tool_list()
+            execute_python = execute_python_tool()
+            
 
             # Set LLM 
-            llm = GoogleGenAI(model="gemini-3.5-flash")
+            llm = GoogleGenAI(model="gemini-3.6-flash")
 
             self.jarmiss = ReActAgent(
                 name="Jarmiss",
                 description="A Personal Agent capably of solving easy tasks from the GAIA dataset",
-                system_prompt="TO-DO",
-                tools=[execute_python_tool],
-                verbose=True, # To see thoughts and observations
-                max_iterations=10,
+                system_prompt = (
+                    "You are a helpful AI agent. "
+                    "When calling tools, you MUST provide strictly valid JSON in Action Input. "
+                    "Ensure all double quotes and newlines within Python code string arguments are properly escaped."
+                ),
+                tools=[execute_python],
+                timeout=120,
                 llm=llm
             )
 
@@ -38,11 +42,36 @@ class Jarmiss:
             print("Error while initializing Jarmiss: ", e)   
             sys.exit()
 
+    async def _solve(self, question: str):
+        """Runs the agent inside a live event loop"""
+        handler = self.jarmiss.run(user_msg=question)
+
+        # Listen to the event stream
+        async for ev in handler.stream_events():
+            # THOUGHT and ACTION
+            if isinstance(ev, AgentStream):
+                print(f"{ev.delta}", end="", flush=True)
+
+            # OBSERVATION
+            elif isinstance(ev, ToolCallResult):
+                print(f"\n\n [OBSERVATION]")
+                print(f"Tool executed: {ev.tool_name}")
+                print(f"Tool_input: {ev.tool_kwargs}")
+                print(f"Result: {ev.tool_output}")
+                print("-" * 40)
+
+        response = await handler
+        return response
+
+
     def __call__(self, question: str):
-        """Solves A given question"""
-        self.agent()
+        """Solves a given question"""
+        print("question: ", question)
+        response = asyncio.run(self._solve(question))
+        print("--------------------------")
+        print(str(response))
 
 agent = Jarmiss()
-agent
+agent("What is 10/2, solve using python")
 
     
